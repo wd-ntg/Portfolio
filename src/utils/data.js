68cea80d0e8f97ff2b36a42ec722087394057fd9
wd-ntg <giangnguyen6777@gmail.com>
2024-12-21 15:56:26 +0700

    Change url Nav

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,7 +1,7 @@
 export const linkButtons = [
   {
     id: 1,
-    link: "https://portfoliowd-en.vercel.app",
+    link: "https://portfolio-up.onrender.com/",
     svg: (
       <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
@@ -18,7 +18,7 @@ export const linkButtons = [
   },
   {
     id: 2,
-    link: "https://portfoliowd-en.vercel.app/about",
+    link: "https://portfolio-up.onrender.com/about",
     svg: (
       <svg
         fill="#ffffff"
@@ -46,7 +46,7 @@ export const linkButtons = [
   },
   {
     id: 3,
-    link: "https://portfoliowd-en.vercel.app/project",
+    link: "https://portfolio-up.onrender.com/project",
     svg: (
       <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
@@ -184,7 +184,7 @@ export const linkButtons = [
   },
   {
     id: 8,
-    link: "https://portfoliowd-en.vercel.app/contact",
+    link: "https://portfolio-up.onrender.com/contact",
     svg: (
       <svg
         fill="#ffffff"
